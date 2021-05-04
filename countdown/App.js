cacae1bc61fd6fee689cb04d83cbb44f686e571a
Lingani Tshuma <tshumal@gmail.com>
2021-05-04 06:04:57 +0200

    countdown - initial commit

---
+++ countdown/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import EventList from './src/screens/EventList'
+
+export default function App() {
+  return (
+<EventList />
+  );
+}
+
+
